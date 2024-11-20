38f718c982212aea5c1451aa2038e2aa7e2b6879
CharanAyyala <charanahn2427@gmail.com>
2024-11-20 18:58:47 +0530

    Update mega project.py

--- mega project.py
+++ mega project.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
             codePath = "C:\\Users\\Hari\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'email to harry' in query:
+        elif 'email to hari' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
